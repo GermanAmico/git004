033de70936fa07b6bcb9b82cf2c4bab210858637
GermanAmico <germanamico@hotmail.com>
2020-10-13 20:03:39 -0300

    Initial commit

---
+++ src/main/java/com/tpa/git/Git004Application.java
@@ -0,0 +1,13 @@
+package com.tpa.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git004Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Git004Application.class, args);
+	}
+
+}
